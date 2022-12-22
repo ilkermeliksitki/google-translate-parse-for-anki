1d17ac52914d02daf0bbc578e956106f14951a40
ilker m. sitki <ilkermeliksitki@proton.me>
2022-12-22 13:26:53 +0300

    add tr-de option

--- google-translate-parser.py
+++ google-translate-parser.py
@@ -24,6 +24,7 @@ de_tr.to_csv("~/Desktop/de-tr.csv", index=False, header=False)
 #print(tr_en)
 tr_en.to_csv("~/Desktop/tr-en.csv", index=False, header=False)
 #print(tr_de)
+tr_de.to_csv("~/Desktop/tr-de.csv", index=False, header=False)
 
 lim = 51 
 for i in range(1, lim):
